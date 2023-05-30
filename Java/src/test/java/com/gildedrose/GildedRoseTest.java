8649913b7ac776e3c73c26872a5aa6c6aff40e70
GD0B9A5L <guillaume-externe.david@enedis.fr>
2023-05-30 17:30:04 +0200

    keep only java

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,15 +3,29 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.*;
+
 
 class GildedRoseTest {
+
+    Item[] items = new Item[] {
+        new Item("+5 Dexterity Vest", 10, 20), //
+        new Item("Aged Brie", 2, 0), //
+        new Item("Elixir of the Mongoose", 5, 7), //
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+        new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        // this conjured item does not work properly yet
+        new Item("Conjured Mana Cake", 3, 6) };
 
     @Test
     void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertThat( app.items[0].name).isEqualTo("fixme");
     }
 
 }
