ca03dc96fe27b56d0f383d45fd7b6c304f632eac
GD0B9A5L <guillaume-externe.david@enedis.fr>
2023-05-30 17:34:20 +0200

    golden test added
    update
    
    
    update

--- Java/src/test/java/com/gildedrose/GildedRoseGolden.java
+++ Java/src/test/java/com/gildedrose/GildedRoseGolden.java
@@ -1,16 +1,16 @@
 package com.gildedrose;
 
-class GildedRose {
+class GildedRoseGolden {
     Item[] items;
 
-    public GildedRose(Item[] items) {
+    public GildedRoseGolden(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,74 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GildedRoseTest {
 
+    Item[] items = new Item[]{
+        new Item("+5 Dexterity Vest", 10, 20), //
+        new Item("Aged Brie", 2, 0), //
+        new Item("Elixir of the Mongoose", 5, 7), //
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+        new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        // this conjured item does not work properly yet
+        new Item("Conjured Mana Cake", 3, 6)};
+
+    Item[] itemsGolden = new Item[]{
+        new Item("+5 Dexterity Vest", 10, 20), //
+        new Item("Aged Brie", 2, 0), //
+        new Item("Elixir of the Mongoose", 5, 7), //
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+        new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        // this conjured item does not work properly yet
+        new Item("Conjured Mana Cake", 3, 6)};
+
+
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertThat(app.items[0].name).isEqualTo("foo");
     }
+
+    @Test
+    void Oneday() {
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items).extracting(item -> item.sellIn).containsExactly(9, 1, 4, 0, -1, 14, 9, 4, 2);
+        assertThat(app.items).extracting(item -> item.quality).containsExactly(19, 1, 6, 80, 80, 21, 50, 50, 5);
+    }
+
+    @Test
+    void Twoday() {
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        assertThat(app.items).extracting(item -> item.sellIn).containsExactly(8, 0, 3, 0, -1, 13, 8, 3, 1);
+        assertThat(app.items).extracting(item -> item.quality).containsExactly(18, 2, 5, 80, 80, 22, 50, 50, 4);
+    }
+
+    @Test
+    void GoldenTest100Loop() {
+        GildedRose app = new GildedRose(items);
+        GildedRoseGolden appGolden = new GildedRoseGolden(itemsGolden);
+
+        for (int day = 0; day < 100; day++) {
+            app.updateQuality();
+            appGolden.updateQuality();
+            assertThat(app.items).extracting(item -> item.sellIn).containsExactly(Arrays.stream(appGolden.items).map(item -> item.sellIn).toArray(Integer[]::new));
+            assertThat(app.items).extracting(item -> item.quality).containsExactly(Arrays.stream(appGolden.items).map(item -> item.quality).toArray(Integer[]::new));
+        }
+    }
+
 
 }
